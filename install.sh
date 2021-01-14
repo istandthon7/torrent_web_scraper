1d186d70f171e254e1acf00b8a90f1a4e298c962
istandthon7 <42507335+istandthon7@users.noreply.github.com>
2021-01-14 16:16:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,4 @@ pip3 install requests
 pip3 install beautifulsoup4
 pip3 install -r requirements.txt
 
-echo "./torrent_web_scraper_install.py 를 실행하세요"
-echo "./torrent_web_scraper.py 를 실행하세요"
-echo "cron에 등록하여 사용할 수 있습니다."
+./torrent_web_scraper_install.py
