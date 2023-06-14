d4c01ef069e2d20ce6e61ced9e47cbf2f30bccb2
jhkim.dt <jhkim@wnote.kr>
2023-06-14 11:55:58 +0900

    키워드 제거 후 줄바꿈이 추가되는 버그 수정

--- movie.py
+++ movie.py
@@ -3,41 +3,40 @@ import logging
 import os
 
 class Movie(stringHelper.StringHelper):
+    keywords = []
+
     def __init__(self, configDirPath: str, movieSetting: dict): #mySetting.json['movie']
         self.movieSetting = movieSetting
         self.listFileName = os.path.join(configDirPath, self.movieSetting['list'])
-        self.keywords = []
         if os.path.exists(self.listFileName):
             with open(self.listFileName, "r", encoding="utf-8") as f:
-                self.keywords = list(filter(None, f.readlines()))
+                self.keywords = [line.rstrip("\r\n") for line in f if line.strip()]
 
     def removeLineInMovieDotTxt(self, regKeyword: str) -> None:
         """Movie.txt에서 삭제하기"""
         if not regKeyword:
             return
 
-        # self.keywords에서 regKeyword가 포함된 키워드를 제외한 나머지 키워드들을 선택합니다.
-        self.keywords = list(filter(lambda x: regKeyword not in x, self.keywords))
-
-        # 선택된 키워드들을 파일에 저장합니다.
-        with open(self.listFileName, "w", encoding="utf-8") as f:
-            f.write('\n'.join(self.keywords))
+        # self.keywords에서 regKeyword가 포함된 키워드를 찾아서 제거합니다.
+        for i, keyword in enumerate(self.keywords):
+            if regKeyword in keyword:
+                del self.keywords[i]
+                with open(self.listFileName, "w", encoding="utf-8") as f:
+                    f.write('\n'.join(self.keywords))
+                break
 
     def getRegKeyword(self, boardTitle: str)->str:
         """
         게시판 제목에 등록된 키워드가 포함되어 있으면 키워드를 반환한다.
         등록되어 있지 않다면 빈문자열을 반환한다.
         """
-        for keyword in self.keywords:
-            keyword = keyword.rstrip("\r\n")
-            searchKeyword = keyword.lower()
-            if searchKeyword == "":
-                continue
-            searchKeyword = searchKeyword.replace(":", " ")
+        filtered_keywords = filter(lambda keyword: keyword.rstrip("\r\n").replace(":", " ") != "", map(lambda keyword: keyword.lower(), self.keywords))
+
+        for keyword in filtered_keywords:
             if 'exclude' in self.movieSetting and self.movieSetting['exclude'] and self.IsContainAnyCommaSeparatedWordsInBoardTitle(self.movieSetting['exclude'], boardTitle):
                 logging.info(f"[{keyword}] 제외 키워드가 포함되어 있어요. [{self.movieSetting['exclude']}]")
                 continue
-            if not self.IsContainAllWordsInBoardTitle(searchKeyword, boardTitle):
+            if not self.IsContainAllWordsInBoardTitle(keyword, boardTitle):
                 logging.debug(f'Movie 키워드에 해당하지 않습니다.')
                 continue
             if not self.IsContainAllWordsInBoardTitle(str(self.movieSetting['resolution']), boardTitle):
