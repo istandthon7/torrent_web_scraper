d6c6695e7e387bd66766a97362d2068b48f7272f
jhkim.dt <jhkim@wnote.kr>
2023-07-10 16:00:49 +0900

    인스톨러 정리

--- scraperInstaller.py
+++ scraperInstaller.py
@@ -8,60 +8,53 @@ class ScraperInstaller:
 
     mySetting = setting.Setting()
 
-    def copyConfigIfNotExist(self, fullPath: str) -> bool:
+    def __init__(self, configDir: str = None) -> None:
+        if configDir is not None:
+            self.mySetting.configDirPath = os.path.join(self.mySetting.currentPath, configDir)
+            self.mySetting.settingPath = os.path.join(self.mySetting.configDirPath, "setting.json")
+
+    def copyFileIfNotExist(self, fullPath: str, isSample: bool = True) -> bool:
         """
         파일이 없으면 .sample파일을 복사하고 true를 반환한다.
         파일이 있으면 false를 반환한다.
         """
         if os.path.isfile(fullPath) is False:
-            shutil.copyfile("./"+os.path.basename(fullPath)+".sample", fullPath)
+            dirPath = os.path.dirname(fullPath)
+            if not os.path.exists(dirPath):
+                os.makedirs(dirPath)
+            sampleFile = os.path.join('.', os.path.basename(fullPath))
+            if isSample:
+                sampleFile += ".sample"
+            shutil.copyfile(sampleFile, fullPath)
             return True
         return False
 
-    def copyPythonFileIfNotExist(self, fullPath: str) -> bool:
-        """
-        파일이 없으면 복사하고 true를 반환한다.
-        파일이 있으면 false를 반환한다.
-        """
-        if os.path.isfile(fullPath) is False:
-            shutil.copyfile("./"+os.path.basename(fullPath), fullPath)
-            osHelper.addXToUser(fullPath)
-            return True
-        return False
 
     def installConfig(self)->None:
-        
         if os.path.isdir(self.mySetting.configDirPath) is False:
             os.mkdir(self.mySetting.configDirPath)
         # setting.json
-        if self.copyConfigIfNotExist(self.mySetting.settingPath):
+        if self.copyFileIfNotExist(self.mySetting.settingPath):
             print("\n\ntransmission 연결정보를 "+ self.mySetting.settingPath+"에 설정해주세요\n")
         self.mySetting.loadJson()
 
         # Movie.json
-        movieListPath = self.mySetting.configDirPath + self.mySetting.json['movie']['list']
-        if self.copyConfigIfNotExist(movieListPath):
+        movieListPath = os.path.join(self.mySetting.configDirPath, self.mySetting.json['movie']['list'])
+        if self.copyFileIfNotExist(movieListPath):
             print("다운로드할 영화를 "+movieListPath+"에 추가하세요.\n")
         # TvShow.json
-        tvShowListPath = self.mySetting.configDirPath + self.mySetting.json["tvshow"]["list"]
-        if self.copyConfigIfNotExist(tvShowListPath) :
+        tvShowListPath = os.path.join(self.mySetting.configDirPath, self.mySetting.json["tvshow"]["list"])
+        if self.copyFileIfNotExist(tvShowListPath) :
             print("다운로드할 tv프로그램을 "+tvShowListPath+"에 추가하세요.\n")
 
     def installTransmissionScript(self)->None:
-        mySetting = setting.Setting()
-        # 시즌 rename: 기본적으로 폴더명이 tvshow일 경우에 작동함
-        if os.path.isdir(mySetting.transmissionScriptDirPath) is False:
-            os.mkdir(mySetting.transmissionScriptDirPath)
-        if self.copyConfigIfNotExist(mySetting.torrentDoneSHPath):
-            osHelper.addXToUser(mySetting.torrentDoneSHPath)
-        self.copyPythonFileIfNotExist(mySetting.renameSeasonTransmissionPYPath)
-        self.copyPythonFileIfNotExist(mySetting.scraperLibraryPYPath)
-        self.copyPythonFileIfNotExist(mySetting.settingPYPath)
+        if os.path.isdir(self.mySetting.transmissionScriptDirPath) is False:
+            os.mkdir(self.mySetting.transmissionScriptDirPath)
+        if self.copyFileIfNotExist(self.mySetting.torrentDoneSHPath):
+            osHelper.addXToUser(self.mySetting.torrentDoneSHPath)
 
 if __name__ == '__main__':
     installer = ScraperInstaller()
     installer.installConfig()
     installer.installTransmissionScript()
     print("설치가 완료되었습니다.\n")
-
-
--- test/test_scraperInstaller.py
+++ test/test_scraperInstaller.py
@@ -6,51 +6,68 @@ import setting
 
 class ScraperInstallerTest(unittest.TestCase): 
 
+    # def setUp(self):
+    #     self.mySetting = setting.Setting(configDir="config_test")
+    #     if not os.path.exists(self.mySetting.configDirPath):
+    #         os.makedirs(self.mySetting.configDirPath)
+    #     self.installer = scraperInstaller.ScraperInstaller()
+
+    # def tearDown(self):
+    #     if os.path.exists(self.mySetting.configDirPath):
+    #         shutil.rmtree(self.mySetting.configDirPath)
+
     def test_copyConfigIfNotExist_파일이_없을때(self):
         # arrange
-        mySetting = setting.Setting()
-        if os.path.isfile(mySetting.settingPath):
-            os.remove(mySetting.settingPath)
+        currentPath = os.path.realpath(os.path.dirname(__file__))
+        testPath = os.path.join(currentPath, "config_test", "setting.json")
         # action
-        installer = scraperInstaller.ScraperInstaller()
-        
+        if os.path.isfile(testPath):
+            os.remove(testPath)
         # assert
-        self.assertTrue(installer.copyConfigIfNotExist(mySetting.settingPath))
+        installer = scraperInstaller.ScraperInstaller()
+        self.assertTrue(installer.copyFileIfNotExist(testPath))
+        dirPath = os.path.dirname(testPath)
+        if os.path.exists(dirPath):
+            shutil.rmtree(dirPath)
 
     def test_copyConfigIfNotExist_파일이_있을때(self):
         # arrange
-        mySetting = setting.Setting()
-        if os.path.isfile(mySetting.settingPath) is False:
-            with open(mySetting.settingPath, 'w') as f:
-                f.close()
+        currentPath = os.path.realpath(os.path.dirname(__file__))
+        testPath = os.path.join(currentPath, "config_test", "setting.json")
+        dirPath = os.path.dirname(testPath)
         # action
-        installer = scraperInstaller.ScraperInstaller()
-        
+        if os.path.isfile(testPath) is False:
+            if not os.path.exists(dirPath):
+                os.makedirs(dirPath)
+            with open(testPath, 'w') as f:
+                f.close()
         # assert
-        self.assertFalse(installer.copyConfigIfNotExist(mySetting.settingPath))
+        installer = scraperInstaller.ScraperInstaller()
+        self.assertFalse(installer.copyFileIfNotExist(testPath))
+        if os.path.exists(dirPath):
+            shutil.rmtree(dirPath)
 
     def test_installConfig_폴더가_없으면_생성되나(self):
         # arrange
-        mySetting = setting.Setting()
-        installer = scraperInstaller.ScraperInstaller()
-        if os.path.isdir(mySetting.configDirPath):
-            shutil.rmtree(mySetting.configDirPath)
+        installer = scraperInstaller.ScraperInstaller(configDir="config_test")
+        if os.path.isdir(installer.mySetting.configDirPath):
+            shutil.rmtree(installer.mySetting.configDirPath)
         # action 
         installer.installConfig()
         #assert
-        self.assertTrue(os.path.isdir(mySetting.configDirPath))
+        self.assertTrue(os.path.isdir(installer.mySetting.configDirPath))
+        if os.path.exists(installer.mySetting.configDirPath):
+            shutil.rmtree(installer.mySetting.configDirPath)
 
     def test_installTransmissionScript_폴더가_없으면_생성되나(self):
         # arrange
-        mySetting = setting.Setting()
-        installer = scraperInstaller.ScraperInstaller()
-        if os.path.isdir(mySetting.transmissionScriptDirPath):
-            shutil.rmtree(mySetting.transmissionScriptDirPath)
+        installer = scraperInstaller.ScraperInstaller(configDir="config_test")
+        if os.path.isdir(installer.mySetting.transmissionScriptDirPath):
+            shutil.rmtree(installer.mySetting.transmissionScriptDirPath)
         # action 
         installer.installTransmissionScript()
         #assert
-        self.assertTrue(os.path.isdir(mySetting.transmissionScriptDirPath))
+        self.assertTrue(os.path.isdir(installer.mySetting.transmissionScriptDirPath))
 
-        
 if __name__ == '__main__':  
     unittest.main()
