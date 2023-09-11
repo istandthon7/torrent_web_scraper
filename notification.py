50f3a5bea7a6f681f63de4651140b840429302e4
jhkim.dt <jhkim@wnote.kr>
2023-09-11 16:32:59 +0900

    notiHistory파일 config 폴더에 생성 #61

--- notification.py
+++ notification.py
@@ -5,53 +5,64 @@ import subprocess
 import datetime
 from typing import List, Dict, Any
 
+# Constants
+HISTORY = "history"
+CMD = "cmd"
+KEYWORDS = "keywords"
+ENCODING = "utf-8"
 
 class Notification:
     notifications: List[List[str]] = []
 
-    def __init__(self, notiSetting: Dict[str, Any]):
+    def __init__(self, configDirPath: str, notiSetting: Dict[str, Any]):
         self.notiSetting = notiSetting  # mySetting.json["notification"]
-        if not os.path.isfile(notiSetting["history"]):
+        self.historyFilePath = os.path.join(configDirPath, self.notiSetting[HISTORY])
+        if not os.path.isfile(self.historyFilePath):
+            logging.error(f"File {self.historyFilePath} not found")
             return
-        with open(self.notiSetting["history"], 'r', encoding="utf-8") as f:
+        with open(self.historyFilePath, 'r', encoding=ENCODING) as f:
             self.notifications = list(csv.reader(f))
 
     def executeNotiScript(self, siteName: str, boardTitle: str) -> bool:
         """
-        스크립트를 실행했으면 true를 리턴하고
-        이미 실행한 경우나 실행하지 못한 경우는 false를 리턴한다.
+        Execute the notification script.
+        If the script is executed, return True.
+        If the script has already been executed or cannot be executed, return False.
         """
         if not self.isValidNotiSetting():
             return False
 
-        for keyword in self.notiSetting["keywords"]:
+        for keyword in self.notiSetting[KEYWORDS]:
             if keyword in boardTitle:
                 if self.checkNotiHistory(boardTitle):
+                    logging.debug(f"Already in the notification history. [{boardTitle}]")
                     return False
                 try:
-                    cmd = self.notiSetting["cmd"]
+                    cmd = self.notiSetting[CMD]
                     cmd = cmd.replace("$board_title", "[" + siteName + "]" + boardTitle.replace("'", "`"))
-                    # check가 참이고, 프로세스가 0이 아닌 종료 코드로 종료되면, CalledProcessError 예외가 발생합니다.
-                    subprocess.run(cmd, shell=True, check=True)
+                    self.run_command(cmd)
                     self.addNotiHistory(siteName, boardTitle, keyword)
                     return True
-                except subprocess.CalledProcessError as e:
-                    logging.error(f"CalledProcessError: {e}")
-                except FileNotFoundError as e:
-                    logging.error(f"FileNotFoundError: {e}")
-                except (PermissionError, OSError) as e:
-                    logging.error(f"Error: {e}")
                 except Exception as e:
-                    logging.error(f"executeNotiScript error, message==>\n{str(e)}")
+                    logging.error(f"Error executing notification script: {e}")
         return False
+
+    def run_command(self, cmd: str) -> None:
+        """Run a command using subprocess."""
+        try:
+            subprocess.run(cmd, shell=True, check=True)
+            logging.debug(f"run_command: [{cmd}]")
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error running command: {e}")
 
     def isValidNotiSetting(self) -> bool:
         """Check if the notification settings are valid."""
         if not isinstance(self.notiSetting, dict):
-            logging.error("notiSetting must be a dictionary")
+            logging.error("Notification settings must be a dictionary.")
             return False
 
-        if self.notiSetting["cmd"] is None or self.notiSetting["cmd"] == "":
+        if self.notiSetting[CMD] is None or self.notiSetting[CMD] == "":
+            logging.error("Command for notification settings is missing.")
             return False
 
         return True
@@ -60,7 +71,7 @@ class Notification:
         """Check if the given title is already in the notification history."""
         for notification in self.notifications:
             if title == notification[2]:
-                logging.debug(f"이미 알림 내역에 있어요. [{notification[2]}]")
+                logging.info(f"Already in the notification history. [{title}]")
                 return True
         return False
 
@@ -68,7 +79,17 @@ class Notification:
         """Add a new entry to the notification history."""
         runtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         new = [runtime, sitename, title, keyword]
-        with open(self.notiSetting["history"], 'a', newline='\n', encoding="utf-8") as f:
-            writer = csv.writer(f)
-            writer.writerow(new)
-        self.notifications.append(new)
+        try:
+            with open(self.historyFilePath, 'a', newline='\n', encoding=ENCODING) as f:
+                writer = csv.writer(f)
+                writer.writerow(new)
+            self.notifications.append(new)
+            logging.info(f"addNotiHistory. [{new}]")
+        except PermissionError:
+            logging.error(f"Permission denied when trying to write to {self.historyFilePath}")
+        except FileNotFoundError:
+            logging.error(f"File {self.historyFilePath} not found")
+        except OSError as e:
+            logging.error(f"OS error occurred when trying to write to {self.historyFilePath}: {e}")
+        except Exception as e:
+            logging.error(f"Unexpected error occurred when trying to write to {self.historyFilePath}: {e}")
